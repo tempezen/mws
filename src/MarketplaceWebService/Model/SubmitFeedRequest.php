4ba07d12d09a21113fe6e9b7de5466f4d5666da9
John Silkey <jpsilkjr86@gmail.com>
2018-07-16 13:50:07 -0700

    added dirname(__FILE__) before every filename in every instance of require_once() to fix path resolution error, in MarketplaceWebService

--- src/MarketplaceWebService/Model/SubmitFeedRequest.php
+++ src/MarketplaceWebService/Model/SubmitFeedRequest.php
@@ -19,8 +19,8 @@
 /**
  *  @see MarketplaceWebService_Model
  */
-require_once ('MarketplaceWebService/Model.php'); 
-require_once ('MarketplaceWebService/Model/ContentType.php');
+require_once (dirname(__FILE__) . 'MarketplaceWebService/Model.php'); 
+require_once (dirname(__FILE__) . 'MarketplaceWebService/Model/ContentType.php');
     
 
 /**
