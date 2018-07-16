213f7b8e6483b8af837375cd279817b3cd4828d8
John Silkey <jpsilkjr86@gmail.com>
2018-07-16 13:56:48 -0700

    fixed typo in require_once

--- src/MarketplaceWebService/Model/SubmitFeedRequest.php
+++ src/MarketplaceWebService/Model/SubmitFeedRequest.php
@@ -19,8 +19,8 @@
 /**
  *  @see MarketplaceWebService_Model
  */
-require_once (dirname(__FILE__) . 'MarketplaceWebService/Model.php'); 
-require_once (dirname(__FILE__) . 'MarketplaceWebService/Model/ContentType.php');
+require_once (dirname(__FILE__) . '/Model.php'); 
+require_once (dirname(__FILE__) . '/Model/ContentType.php');
     
 
 /**
